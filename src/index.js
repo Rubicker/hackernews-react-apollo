fb7f2002dc99ebc33547b458269ddf75f4edcd4a
Rubicker <newsl00001@gmail.com>
2019-06-05 18:06:25 +0800

    :sparkles: Add react-router

--- src/index.js
+++ src/index.js
@@ -2,29 +2,28 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import React from "react";
-// 1
 import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
-// 2
 const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
-// 3
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache()
 });
 
-// 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 serviceWorker.unregister();
