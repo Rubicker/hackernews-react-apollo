385cc457a5313bb25ba04474a29c0032a9aee718
Rubicker <newsl00001@gmail.com>
2019-06-06 09:32:27 +0800

    :sparkles: Add login

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
+import { setContext } from "apollo-link-context";
 import { createHttpLink } from "apollo-link-http";
 import React from "react";
 import { ApolloProvider } from "react-apollo";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import App from "./components/App";
+import { AUTH_TOKEN } from "./constants";
 import * as serviceWorker from "./serviceWorker";
 import "./styles/index.css";
 
@@ -13,8 +15,18 @@ const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
